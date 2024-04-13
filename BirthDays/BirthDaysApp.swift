62f82032025a4696adc936eff49211df67173f81
thai-ru <robinthairu34@gmail.com>
2024-04-14 02:30:50 +0300

    Initial Commit

---
+++ BirthDays/BirthDaysApp.swift
@@ -0,0 +1,17 @@
+//
+//  BirthDaysApp.swift
+//  BirthDays
+//
+//  Created by Robinson Thairu on 14/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct BirthDaysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
